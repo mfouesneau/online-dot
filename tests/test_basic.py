fc776f0a33f6b73ace5c9d8492425864d291dff0
Morgan Fouesneau <morgan.fouesneau@astro.washington.edu>
2022-08-17 14:59:34 +0200

    unittest

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,10 +1,7 @@
-import pytest
-from onlinedot import dot
-
-def check_basic_run():
+def test_basic_run():
     """
     Basic hello world diagram
     """
+    from onlinedot import dot
     r = dot("digraph G {Hello->World}", T='svg')
     assert r not in [None, '']
-
